60bca1083b731984aa0bd706187cd7cd4a9fe7e8
Mikhail Ikonnikov <mishaikon@gmail.com>
2024-07-25 23:20:19 +0300

    - add labels

--- parse.php
+++ parse.php
@@ -15,21 +15,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $baseDir .= "/" . $parser->getDomainFromUrl($url);
 
     echo '<div class="container">';
+    echo '<h3>Лог парсинга</h3>';
 
-    echo "<p>Начало парсинга ...</p><br />\n";
+    echo "<p>Начало парсинга ...</p>\n";
     if(is_dir($baseDir)) {
         $parser->deleteDirectory($baseDir);
-        echo "<p>Удалена старая папка с файлами от предыдущего парсинга: <br />" . $baseDir . "</p><br />\n";
+        echo "<p>Удалена старая папка с файлами от предыдущего парсинга: <br />" . $baseDir . "</p>\n";
     }
 
     // Do parsing
     $parser->downloadPage($url, $depth, $baseDir);
 
-    echo "<p>Парсинг успешно выполнен.</p><br />\n";
+    echo "<p>Парсинг успешно выполнен.</p>\n";
 
     // Display links to downloaded files
     echo '<h3>Скачанные файлы</h3>';
-    echo '<ul>';
+    echo '<ol>';
     $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($baseDir));
     foreach ($iterator as $file) {
         if ($file->isFile()) {
@@ -37,6 +38,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo '<li><a href="' . $filePath . '" target="_blank">' . $filePath . '</a></li>';
         }
     }
-    echo '</ul>';
+    echo '</ol>';
     echo '</div><!-- "container" -->';
 }
